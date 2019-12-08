8a0a262d197566e3725a6205b3f35491ba7285b3
theMkrv <themkrv@gmail.com>
2019-12-08 22:09:15 +0700

    Фтфд

--- main.py
+++ main.py
@@ -21,34 +21,37 @@ for i in site.select('.player-quick-stat-item '):
 
 """
 # f = open('players.txt', '+')
+for i in
+    with open("players.txt", "r") as f_pl:
+        str = f_pl.readline(limit=22)
 
-url = 'http://www.nfl.com/player/jimmygaroppolo/2543801/profile'
-response = requests.get(url)
+        url = 'http://www.nfl.com/player/jimmygaroppolo/2543801/profile'
+        response = requests.get(url)
 
-name_position = str(response.text).find('player-name')
-text = str(response.text)
-name_end = text.find('<', name_position)
-name = str(response.text)[name_position + 13:name_end - 12]
+        name_position = str(response.text).find('player-name')
+        text = str(response.text)
+        name_end = text.find('<', name_position)
+        name = str(response.text)[name_position + 13:name_end - 12]
 
 
-def text_func(TDS):
-    tds_pos = str(response.text).find(TDS)
-    text_tds = str(response.text)
-    TDS_1 = text_tds.find('player-quick-stat-item-body', tds_pos)
-    TDS_2 = text_tds.find('player-quick-stat-item-body', tds_pos)
-    print(TDS_2)
-    SEND  = text_tds[TDS_1 + 29:TDS_2 + 37].replace('</p', '').replace('>', '').replace(' ', '')
-    #print(text_tds[TDS_1 + 29:TDS_2 + 31])
-    return SEND
+        def text_func(TDS):
+            tds_pos = str(response.text).find(TDS)
+            text_tds = str(response.text)
+            TDS_1 = text_tds.find('player-quick-stat-item-body', tds_pos)
+            TDS_2 = text_tds.find('player-quick-stat-item-body', tds_pos)
+            print(TDS_2)
+            SEND  = text_tds[TDS_1 + 29:TDS_2 + 37].replace('</p', '').replace('>', '').replace(' ', '')
+            #print(text_tds[TDS_1 + 29:TDS_2 + 31])
+            return SEND
 
-int_send = text_func('INT')
-tds_send = text_func('TDS')
-yds_send = text_func('YDS')
-rtg_send = text_func('RTG')
-print('---------------')
-print('Name: ', name)
-print('TDS: ', tds_send)
-print('INT: ', int_send)
-print('YDS: ', yds_send)
-print('RTG: ', rtg_send)
+        int_send = text_func('INT')
+        tds_send = text_func('TDS')
+        yds_send = text_func('YDS')
+        rtg_send = text_func('RTG')
+        print('---------------')
+        print('Name: ', name)
+        print('TDS: ', tds_send)
+        print('INT: ', int_send)
+        print('YDS: ', yds_send)
+        print('RTG: ', rtg_send)
 
